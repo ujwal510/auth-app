e5b9d1e457aaecb63f397af02998f06759fff470
ujwal510 <ujwalreddy9810@gmail.com>
2024-02-02 16:57:22 +0530

    signup api route

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,6 +1,6 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-const userSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema({
     username:{
         type: String,
         required: true,
@@ -20,6 +20,6 @@ const userSchema = new mongoose.Schema({
 
 },{timestamps: true});
 
-const User = mongoose.model('User,userSchema');
+const User = mongoose.model('User', UserSchema);
 
 export default User;
