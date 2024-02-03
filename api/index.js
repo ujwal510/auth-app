6cfe3ffa5b888eb9cdec3b54725a30497b771c07
ujwal510 <ujwalreddy9810@gmail.com>
2024-02-03 11:31:27 +0530

    add a middleware

--- api/index.js
+++ api/index.js
@@ -16,9 +16,21 @@ const app = express();
 app.use(express.json());
 
 app.listen (3000,() => {
-    console.log('Server listening on port 3000')
+    console.log('Server listening on port 300')
 })
 
 
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
+
+
+
+app.use((err,req,res,next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        success: false,
+        message,
+        statusCode
+    });
+});
